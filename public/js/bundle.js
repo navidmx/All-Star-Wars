ac9c1df4500ddcb157f91123e291cd1124f302a5
A-Pujols-ASC <A-Pujols-ASC@users.noreply.github.com>
2016-07-30 18:17:38 -0400

    Score increaes properly

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,3 +1,5 @@
+//placed up here so score wont reset to 0 at every frame
+var score = 0;
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Corridor(textureLoader){
 
@@ -457,7 +459,6 @@ function update(dt){
   }
   // Check collision with lightsaber and enemy at every iteration
   //ALSO updates "score" to document each time enemy is hit
-var score = 0;
 Utils.checkCollision(lightsaber.children[0], "enemy", true, function(result){
   	if(result){
   		socket.emit('sendhit');
