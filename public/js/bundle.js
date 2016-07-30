f99f262e6117063527da873bc5a64ac929c2df8d
A-Pujols-ASC <A-Pujols-ASC@users.noreply.github.com>
2016-07-30 18:01:59 -0400

    Renamed sky texture to sky1 so it wont load

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -134,7 +134,7 @@ function Sky(textureLoader){
 	
 	var skyGeometry = new THREE.SphereGeometry(10000, 10000, 25, 25);
 	var skyMaterial = new THREE.MeshBasicMaterial({
-		map: textureLoader.load('textures/sky.jpg'),
+		map: textureLoader.load('textures/sky1.jpg'),
 		side: THREE.BackSide});
 	var skyDome = new THREE.Mesh(skyGeometry, skyMaterial);
 	skyDome.rotateY(-Math.PI/2);
