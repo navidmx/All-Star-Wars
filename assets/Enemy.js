8c3f430dc48ef02ed505c9be909ef6bd59b0baae
Navid Mamoon <navidmx@gmail.com>
2016-08-02 18:45:29 -0400

    Updating Enemies

--- assets/Enemy.js
+++ assets/Enemy.js
@@ -1,6 +1,6 @@
 function Enemy(){
 	var enemyGeometry = if x %2 = 0 {
-    new THREE.SphereGeometry(2, 40, 40);
+    new THREE.CylinderGeometry(2, 2, 5, 8);
     } 
 	var enemyMaterial = new THREE.MeshBasicMaterial({transparent: true, opacity: 0.75, color: "rgb(Utils.getRandomInRange(0, 255), Utils.getRandomInRange(0, 255), Utils.getRandomInRange(0, 255))"});
 
