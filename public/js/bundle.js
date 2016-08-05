7107ce2fc69ca1bde1b72b04bb177ca8042b8379
Navid Mamoon <navidmx@gmail.com>
2016-08-05 15:35:10 -0400

    Updated Lightsaber Handle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -128,7 +128,7 @@ module.exports = Floor;
 },{}],4:[function(require,module,exports){
 function Hand(camera){
 	/* HAND */
-	var handGeometry = new THREE.CylinderGeometry(.6, .6, 5, 7);
+	var handGeometry = new THREE.CylinderGeometry(.7, .7, 6, 7);
 	var handMaterial = new THREE.MeshBasicMaterial({color: "#545355"});
 	hand = new THREE.Mesh(handGeometry, handMaterial);
 	hand.position.set(15, 6, camera.position.z / 2);
