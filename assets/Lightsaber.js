8ea01cbb3983a40ac5f322660340ba9dd5e71ec2
Adonis Pujols <adonispujols@users.noreply.github.com>
2017-07-10 22:24:27 -0400

    Reverting - Succesful test

--- assets/Lightsaber.js
+++ assets/Lightsaber.js
@@ -1,17 +1,17 @@
-// function Lightsaber(){
-// 	/* LIGHTSABER MODEL */
-// 	var lsGeometry = new THREE.CylinderGeometry(0.4, 0.04, 30, 20);
-// 	var lsMaterial = new THREE.MeshBasicMaterial({ color: "white" });
-// 	lightsaber = new THREE.Mesh( lsGeometry, lsMaterial );
-// 	lightsaber.position.setY(15);
+function Lightsaber(){
+	/* LIGHTSABER MODEL */
+	var lsGeometry = new THREE.CylinderGeometry(0.4, 0.04, 30, 20);
+	var lsMaterial = new THREE.MeshBasicMaterial({ color: "white" });
+	lightsaber = new THREE.Mesh( lsGeometry, lsMaterial );
+	lightsaber.position.setY(15);
 
-// 	var glowGeometry = new THREE.CylinderGeometry(0.5, 0.5, 30, 20);
-// 	var glowMaterial = new THREE.MeshBasicMaterial({transparent: true, opacity: 0.5, color: "#00FFFF" });
-// 	var glow = new THREE.Mesh(glowGeometry, glowMaterial);
+	var glowGeometry = new THREE.CylinderGeometry(0.5, 0.5, 30, 20);
+	var glowMaterial = new THREE.MeshBasicMaterial({transparent: true, opacity: 0.5, color: "#00FFFF" });
+	var glow = new THREE.Mesh(glowGeometry, glowMaterial);
 
-// 	lightsaber.add(glow);
+	lightsaber.add(glow);
 
-// 	return lightsaber;
-// }
+	return lightsaber;
+}
 
-// module.exports = Lightsaber;
+module.exports = Lightsaber;
